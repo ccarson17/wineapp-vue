6325fa3a3005976c10da4537db866aebf1294733
Chris Carson <PoisonEye7@gmail.com>
2026-06-24 22:18:36 -0400

    Update clientId in authConfig.js

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,6 +1,6 @@
 export const msalConfig = {
     auth: {
-        clientId: "28d37e9a-7dc0-4114-883c-889851ccfb81",
+        clientId: "543dbce2-66ac-461f-8391-ce4c745677b5",
         authority: "https://oenofileapp.ciamlogin.com/oenofileapp.onmicrosoft.com",
         knownAuthorities: ["oenofileapp.ciamlogin.com"],
         redirectUri: window.location.origin,
